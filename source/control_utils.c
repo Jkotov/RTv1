2a3eb5af6d9ff960ebb5a4e3a9209b3f90c3c6b8
GingerTaleFX <redishtale@gmail.com>
2020-09-17 18:57:05 +0300

    new menu, without controls

--- source/control_utils.c
+++ source/control_utils.c
@@ -1,30 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   control_utils.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: epainter <epainter@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/17 12:48:35 by epainter          #+#    #+#             */
-/*   Updated: 2020/09/17 18:51:44 by root             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-#include "rtv1.h"
 
-void		mouse_events(t_sdl *sdl, SDL_Event e)
+#include "../include/rtv1.h"
+
+void			mouse_events(t_sdl *sdl, SDL_Event e)
 {
-	if (e.button.y < 100)
+	t_sphere sphere;
+
+	sphere.center = (t_dot){0, 0, 0};
+	sphere.specular = 0;
+	sphere.radius = 0;
+	sphere.color = 0;
+	if (e.button.y < 40)
 	{
-		if (e.button.x > 100 && e.button.x < 200)
+		if (e.button.x < 90)
 		{
-			SDL_RenderCopy(sdl->renderer, sdl->menu.add_menu, NULL,\
-			&sdl->menu.add_menu_size);
-			SDL_RenderPresent(sdl->renderer);
-			while (e.type != SDL_MOUSEBUTTONDOWN || (e.button.x > 100))
-			{
-				SDL_PollEvent(&e);
-			}
+			//			add sphere
+					printf("1\n");
+			//			SDL_RenderCopy(sdl->renderer, sdl->menu.add_menu, NULL, &sdl->menu.add_menu_size);
+//			SDL_RenderPresent(sdl->renderer);
+//			while (e.type != SDL_MOUSEBUTTONDOWN || (e.button.x > 100))
+//			{
+//				SDL_PollEvent(&e);
+//
+//			}
+		}
+		if (e.button.x > 90 && e.button.x < 185)
+		{
+			//			add cone
+			printf("2\n");
+		}
+		if (e.button.x > 185 && e.button.x < 280)
+		{
+			//			add plane
+			printf("3\n");
+		}
+		if (e.button.x > 280 && e.button.x < 390)
+		{
+			//			add cylinder
+			printf("4\n");
+		}
+		if (e.button.x > 390 && e.button.x < 480)
+		{
+//			add light
+			printf("5\n");
+		}
+		if (e.button.x > 480 && e.button.x < 540)
+		{
+//			reset
+			printf("6\n");
 		}
 	}
 }
@@ -49,6 +72,6 @@ void		keyboard_events(t_sdl *sdl, char *quit, SDL_Event e)
 			SDL_PollEvent(&e);
 		add_sphere(&sdl->scene.sphere, (t_sphere){tmp,\
 		abs_vector(vector_subtraction((t_dot){e.button.x, e.button.y, 300},\
-		tmp)), 0xFF00, 100, 0.3, NULL, NULL});
+		tmp)), 0xFF00, 100, 0.3, NULL});
 	}
 }
