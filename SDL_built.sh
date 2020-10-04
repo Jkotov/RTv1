3f126e4a605b029593d7a17cfb8966b5aa63ff53
JKotov <epainter@student.21-school.ru>
2020-10-04 14:39:38 +0300

    SDL_built.sh update

--- SDL_built.sh
+++ SDL_built.sh
@@ -2,6 +2,12 @@
 
 PROJECT_DIR=$(pwd)
 rm -rf ${PROJECT_DIR}/SDL2
+rm -rf ${PROJECT_DIR}/SDL2-2.0.12
+rf -rf ${PROJECT_DIR}/SDL2_image-2.0.5
+rm -rf ${PROJECT_DIR}/SDL2_ttf-2.0.15
+tar xfvz SDL2-2.0.12.tar.gz
+tar xfvz SDL2_image-2.0.5.tar.gz
+tar xfvz SDL2_image-2.0.5.tar.gz
 cd SDL2-2.0.12
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         CC=
@@ -10,6 +16,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 else
 	exit 0;
 fi
+
 rm -rf build
 mkdir build
 cd build
