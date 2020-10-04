9bd67b7a41aecb27e7651c89eeb894a1cb41fd7e
JKotov <epainter@student.21-school.ru>
2020-10-04 14:45:59 +0300

    SDL_built.sh update

--- SDL_built.sh
+++ SDL_built.sh
@@ -7,7 +7,7 @@ rf -rf ${PROJECT_DIR}/SDL2_image-2.0.5
 rm -rf ${PROJECT_DIR}/SDL2_ttf-2.0.15
 tar xfvz SDL2-2.0.12.tar.gz
 tar xfvz SDL2_image-2.0.5.tar.gz
-tar xfvz SDL2_image-2.0.5.tar.gz
+tar xfvz SDL2_ttf-2.0.15.tar.gz
 cd SDL2-2.0.12
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         CC=
